dbbd7f4d9f625c02ac373ea494f26c1de1e251ef
Oleg Pushkarev <tyler98765@yandex.ru>
2021-02-20 19:05:36 +0300

    Изменена инициализация драйвера

--- src/main/java/ru/aristovo/framework/managers/DriverManager.java
+++ src/main/java/ru/aristovo/framework/managers/DriverManager.java
@@ -5,6 +5,11 @@ import org.junit.Assert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URI;
 
 import static ru.aristovo.framework.utils.PropConst.*;
 
@@ -35,12 +40,28 @@ public class DriverManager {
     }
 
     private void initDriver() {
-        if (OS.isFamilyWindows()) {
-            initDriverWindowsOsFamily();
-        } else if (OS.isFamilyMac()) {
-            initDriverMacOsFamily();
-        } else if (OS.isFamilyUnix()) {
-            initDriverUnixOsFamily();
+        switch (props.getProperty(TYPE_BROWSER)) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", props.getProperty(PATH_GECKO_DRIVER_WINDOWS));
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", props.getProperty(PATH_CHROME_DRIVER_WINDOWS));
+                driver = new ChromeDriver();
+                break;
+            case "remote":
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setBrowserName("chrome");
+                capabilities.setVersion("73.0");
+                capabilities.setCapability("enableVNC", true);
+                capabilities.setCapability("enableVideo", false);
+                try {
+                    driver = new RemoteWebDriver(
+                            URI.create("http://selenoid.appline.ru:4445/wd/hub/").toURL(),
+                            capabilities);
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
         }
     }
 
